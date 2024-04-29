6dfc5ec6884edff68a92d4516183126c3ec407b3
DOmi <dominika-090@wp.pl>
2024-04-29 17:39:45 +0200

    Zadanie z dnia 4

--- ChallengeAppDomi/ChallengeAppDomi/Program.cs
+++ ChallengeAppDomi/ChallengeAppDomi/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Domi!");
+﻿int age = 28;
+string name = "Ewa";
+var sex = "W";
+
+if (sex == "W" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat30");
+}
+else if (sex == "M" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
