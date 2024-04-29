410fc909df84c6ba00c444d42f644a5fdd3b5d50
DOmi <dominika-090@wp.pl>
2024-04-30 00:25:46 +0200

    Zadanie z dnia 5

--- ChallengeAppDomi/ChallengeAppDomi/Program.cs
+++ ChallengeAppDomi/ChallengeAppDomi/Program.cs
@@ -1,16 +1,76 @@
-﻿int age = 28;
-string name = "Ewa";
-var sex = "W";
+﻿int number = 22789;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-if (sex == "W" && age < 30)
+int nr0 = 0;
+int nr1 = 0;
+int nr2 = 0;
+int nr3 = 0;
+int nr4 = 0;
+int nr5 = 0;
+int nr6 = 0;
+int nr7 = 0;
+int nr8 = 0;
+int nr9 = 0;
+
+foreach(char sign in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (sign == '0')
+    { 
+        nr0++;
+    }
+    else if (sign == '1')
+    {
+        nr1++;
+    }
+
+    else if (sign == '2')
+    {
+        nr2++;
+    }
+    else if (sign == '3')
+    {
+        nr3++;
+    }
+    else if (sign == '4')
+    {
+        nr4++;
+    }
+    else if (sign == '5')
+    {
+        nr5++;
+    }
+
+    else if (sign == '6')
+    {
+        nr6++;
+    }
+
+    else if (sign == '7')
+    {
+        nr7++;
+    }
+
+    else if (sign == '8')
+    {
+        nr8++;
+    }
+    else if (sign == '9')
+    {
+        nr9++;
+    }
+
 }
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat30");
-}
-else if (sex == "M" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+
+Console.WriteLine($"0=> {nr0}");
+Console.WriteLine($"1=> {nr1}");
+Console.WriteLine($"2=> {nr2}");
+Console.WriteLine($"3=> {nr3}");
+Console.WriteLine($"4=> {nr4}");
+Console.WriteLine($"5=> {nr5}");
+Console.WriteLine($"6=> {nr6}");
+Console.WriteLine($"7=> {nr7}");
+Console.WriteLine($"8=> {nr8}");
+Console.WriteLine($"9=> {nr9}");
+
+
